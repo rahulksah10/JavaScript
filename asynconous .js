27c722caa2874592824cb6e34d41e3b4fc476317
Rahul kumar sah <rahulraj12777@gmail.com>
2025-07-26 15:29:40 +0530

    asyncronous

---
+++ asynconous .js
@@ -0,0 +1,15 @@
+// Syncronous programming
+// const prompt = require("prompt-sync")();
+// let a = prompt("What is your name: ");
+// let b = prompt("What is your age: ");
+// let c = prompt("What is your favarite color: ");
+// console.log(a + " is " + b + " year old, " + c + " is favorite color.");
+
+
+// Asyncronous programming
+ console.log("Hey, ");
+setTimeout( function () {
+    console.log("good to see you");
+    console.log("bye.");
+ }, 3000 )
+ console.log("wait");
