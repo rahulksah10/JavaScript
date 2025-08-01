881b295c8775cadf057068e385cfd926aa419669
Rahul kumar sah <rahulraj12777@gmail.com>
2025-08-01 17:04:27 +0530

    filter

--- reduce.js
+++ reduce.js
@@ -1,16 +1,18 @@
 const arr = [5, 3, 4, 6, 8, 1];
- // sum 
+// sum
 
- function findsum (arr){
-    let x =0 ;
-    for(i=0; i<arr.length; i++){
-       x = x+ arr[i]
-    }
-    return x;
- }
-//  console.log(findsum(arr));
+function findsum(arr) {
+  let x = 0;
+  for (i = 0; i < arr.length; i++) {
+    x = x + arr[i];
+  }
+  return x;
+}
+ console.log(findsum(arr));
 
-
- const arr2 = [15, 3, 1, 5, 8, 6];
- const filt =arr2.filter(findsum(arr))
- console.log(filt);
+const arr2 = [15, 3, 1, 5, 8, 6];
+const filt = arr2.reduce(function (acc, curr) {
+  acc = acc + curr;
+  return acc;
+});
+console.log(filt);
